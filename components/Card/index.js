9e57d5b13e3cd992cfef71702c522a26aa26c1ce
Franziska Adam <franziskacarolineadam@gmail.com>
2023-12-12 11:14:31 +0100

    Update Styling Card Component

--- components/Card/index.js
+++ components/Card/index.js
@@ -6,7 +6,7 @@ export default function PlantCard() {
   return (
     <StyledList>
       {plants.map((plant) => (
-        <StyledListItem key={plant.id}>
+        <li key={plant.id}>
           <StyledFigure>
             <Image
               src={plant.image}
@@ -14,9 +14,9 @@ export default function PlantCard() {
               height={150}
               alt={plant.commonName}
             />
-            <figcaption>{plant.commonName}</figcaption>
+            <StyledCaption>{plant.commonName}</StyledCaption>
           </StyledFigure>
-        </StyledListItem>
+        </li>
       ))}
     </StyledList>
   );
@@ -29,21 +29,17 @@ const StyledList = styled.ul`
   gap: 1rem;
 `;
 
-const StyledListItem = styled.li`
-  //   width: 9rem;
-  //   height: 13rem;
-  //   border-radius: 1rem;
-  //   border: 2px solid #e6e6e6;
-  //   overflow: hidden;
-  //   background-color: var(--color-beige);
-  //
-`;
-
 const StyledFigure = styled.figure`
+  margin: 0;
   width: 9rem;
   height: 13rem;
   border-radius: 1rem;
   border: 2px solid #e6e6e6;
   overflow: hidden;
   background-color: var(--color-beige);
+`;
+
+const StyledCaption = styled.figcaption`
+  text-align: center;
+  margin: 0.25rem;
 `;
