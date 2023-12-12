f5ef95863cbb8073c4625ad39c6deb4eeafa646c
Franziska Adam <franziskacarolineadam@gmail.com>
2023-12-12 11:17:19 +0100

    Update Style on Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import PlantCard from "@/components/Card";
-
+import styled from "styled-components";
 
 export default function HomePage() {
   return (
     <>
-      <h1>PlantBuddy</h1>
-      <PlantCard />    
+      <StyledHeader>PlantBuddy</StyledHeader>
+      <PlantCard />
     </>
   );
 }
+
+const StyledHeader = styled.h1`
+  text-align: center;
+  color: var(--color-green);
+  font-family: Newsreader;
+  font-size: 3rem;
+`;
