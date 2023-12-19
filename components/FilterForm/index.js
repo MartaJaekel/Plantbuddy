215df600003a0575e8a8c78027a7cdceef501809
Franziska Adam <franziskacarolineadam@gmail.com>
2023-12-19 10:15:12 +0100

    Update Filter Form

--- components/FilterForm/index.js
+++ components/FilterForm/index.js
@@ -44,45 +44,58 @@ export default function FilterForm({ onFilterUpdate, plants }) {
 
   return (
     <StyledForm onSubmit={handleFilter} onReset={handleReset}>
+      <StyledLabel for="plantSize">Select the plant size:</StyledLabel>
       <StyledSelect
         name="plantSize"
-        onChange={(e) => setPlantSize(e.target.value)}
+        onChange={(event) => setPlantSize(event.target.value)}
       >
         <option value="">Select Size</option>
-        <option value="Small">Small (15cm-50cm)</option>
-        <option value="Medium">Medium (50cm-90cm)</option>
-        <option value="Large">Large (90cm+)</option>
+        <option value="small">Small (15cm-50cm)</option>
+        <option value="medium">Medium (50cm-90cm)</option>
+        <option value="large">Large (90cm+)</option>
       </StyledSelect>
+
+      <StyledLabel for="sunlightRequirement">
+        Select the sunlight Requirement:
+      </StyledLabel>
       <StyledSelect
         name="sunlightRequirement"
-        onChange={(e) => setSunlightRequirement(e.target.value)}
+        onChange={(event) => setSunlightRequirement(event.target.value)}
       >
         <option value="">Select Sunlight Requirement</option>
-        <option value="Full Sun">Full Sun</option>
-        <option value="Indirect Light">Indirect Light</option>
-        <option value="Half-Shade">Half-Shade</option>
+        <option value="full sun">Full Sun</option>
+        <option value="indirect light">Indirect Light</option>
+        <option value="half-shade">Half-Shade</option>
       </StyledSelect>
+
+      <StyledLabel for="waterNeeds">Select the water needs:</StyledLabel>
       <StyledSelect
         name="waterNeeds"
-        onChange={(e) => setWaterNeeds(e.target.value)}
+        onChange={(event) => setWaterNeeds(event.target.value)}
       >
         <option value="">Select Water Needs</option>
-        <option value="Weekly">Weekly</option>
-        <option value="Every other week">Every other week</option>
-        <option value="Monthly">Monthly</option>
+        <option value="weekly">Weekly</option>
+        <option value="every other week">Every other week</option>
+        <option value="monthly">Monthly</option>
       </StyledSelect>
+
+      <StyledLabel for="optimalTemperature">
+        Select the optimal temperature:
+      </StyledLabel>
       <StyledSelect
         name="optimalTemperature"
-        onChange={(e) => setOptimalTemperature(e.target.value)}
+        onChange={(event) => setOptimalTemperature(event.target.value)}
       >
         <option value="">Select Temperature</option>
-        <option value="15-20°C">15-20°C</option>
-        <option value="18-25°C">18-25°C</option>
-        <option value="20-30°C">20-30°C</option>
+        <option value="low">15-20°C</option>
+        <option value="average">18-25°C</option>
+        <option value="high">20-30°C</option>
       </StyledSelect>
+
+      <StyledLabel for="petFriendly">Select the pet compatibility:</StyledLabel>
       <StyledSelect
         name="petFriendly"
-        onChange={(e) => setPetFriendly(e.target.value)}
+        onChange={(event) => setPetFriendly(event.target.value)}
       >
         <option value="">Select Pet Compatibility</option>
         <option value="true">Yes</option>
@@ -97,6 +110,7 @@ export default function FilterForm({ onFilterUpdate, plants }) {
 }
 
 const StyledForm = styled.form`
+  position: relative;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
@@ -104,6 +118,19 @@ const StyledForm = styled.form`
   gap: 1rem;
   max-width: 19rem;
   margin: 1rem auto 1rem auto;
+`;
+
+const StyledLabel = styled.label`
+  border: 0;
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  white-space: nowrap;
 `;
 
 const StyledSelect = styled.select`
