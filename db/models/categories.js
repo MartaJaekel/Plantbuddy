7e62bc942f352d70e578f843907df12dab820ccf
Jan Maaß <git@antipost.de>
2024-01-09 15:41:06 +0100

    Add db connection and category db model

---
+++ db/models/categories.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  slug: { type: String, required: true },
+  title: { type: String, required: true },
+  bgcolor: { type: String, required: true },
+  image: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+const Category = mongoose.models.Category || mongoose.model("Category", categorySchema);
+
+export default Category;
