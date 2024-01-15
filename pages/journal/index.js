bcc1f116e9e13c8a796043063b2d53993a756be2
Marta Jäkel <martajaekel@gmail.com>
2024-01-15 15:10:44 +0100

    take out excessive back button

--- pages/journal/index.js
+++ pages/journal/index.js
@@ -7,22 +7,10 @@ import Image from "next/image";
 import EntryCard from "@/components/JournalEntryCard";
 
 export default function JournalOverviewPage({ entries }) {
-  const router = useRouter();
-  const goBack = () => {
-    router.back();
-  };
-
   return (
     <>
       <StyledHeadline>PlantBuddy</StyledHeadline>
-      <StyledBackButton type="button" aria-label="Go Back" onClick={goBack}>
-        <Image
-          src="/assets/ArrowIcon.svg"
-          alt="Back Link"
-          width={25}
-          height={20}
-        />
-      </StyledBackButton>
+
       <main>
         <StyledTitle>Plant Journal </StyledTitle>
         <StyledLink href="/journal/entry">
