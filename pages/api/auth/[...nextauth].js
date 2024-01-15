b851adc34af33de74ac45ef08b6c3ea2e9ba7458
Franziska Adam <franziskacarolineadam@gmail.com>
2024-01-15 15:54:26 +0100

    Add credentials provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,7 +9,26 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "plant" },
+        password: { label: "Password", type: "password", placeholder: "buddy" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "plant" &&
+          credentials.password === "buddy"
+        ) {
+          return {
+            name: "Buddy",
+          };
+        } else {
+          return <h2>"Wrong Username or Password"</h2>;
+        }
+      },
+    }),
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
